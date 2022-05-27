d6466eee74b40ff22872fe5bdcc751b5a7912664
Alan Lopes <desenv.alanlopes@gmail.com>
2022-05-27 19:56:15 -0300

    add: script para remocao das caixas desabilitadas de todas as equipes do MS Teams

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Script para emove as caixas compartilhada (SharedMailbox) de todos os as equipes do MS Teams
+# Scripts para manutenção das equipes do MS Teams
+
+#### - Lista dos scripts
+- RemoveSharedMailboxFromAllTeams.ps1   -- Remove de todos as equipes as caixas de e-mail compartilhadas (SharedMailbox).
+- RemoveDisabledMailboxFromAllTeams.ps1 -- Remove de todos as equipes as caixas de e-mail bloqueadas (Mailbox).
--- RemoveDisabledMailboxFromAllTeams.ps1
+++ RemoveDisabledMailboxFromAllTeams.ps1
@@ -15,14 +15,14 @@ Connect-ExchangeOnline -Credential $var_credential -ShowProgress $true
 # Inicia conexao com Teams
 Connect-MicrosoftTeams -Credential $var_credential
 
-# Lista todos as caixas de emails compartilhada (SharedMailbox)
+# Lista todos as caixas de emails desabilitadas (SharedMailbox)
 $var_mailbox = Get-MsolUser -All | Where {$_.BlockCredential -eq $True} | Select UserPrincipalName
 
 # Variaveis de controle
 $var_count_total = ($var_mailbox).Count
 $var_count = 1
 
-#Excluir caixa de email compartilhda das equipes do MS Teams
+#Excluir caixa de email desabilitadas das equipes do MS Teams
 Foreach ($var_user in $var_mailbox) {
     If ((Get-Team -User $var_user.UserPrincipalName) -ne $null) {
         $teams = Get-Team -User $var_user.UserPrincipalName
